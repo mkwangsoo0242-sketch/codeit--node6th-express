ca8e34aaeed1e6a1dc89c2919fb3c9e480bc9876
mkwangsoo <mkwangsoo@naver.com>
2025-10-20 12:40:02 +0900

    express 설치및 시작

---
+++ app.js
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const PORT = 3000;
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Hello Express');
+});
+
+app.listen(PORT, () => {
+  console.log('Server Started');
+});
